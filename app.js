f45a06843057cdceb27f91e532ba9da5384f758f
Michael <foxhuntintx@gmail.com>
2021-11-28 21:19:40 -0600

    initial project for Liberty Hill - skeleton

---
+++ app.js
@@ -0,0 +1,28 @@
+const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser'); 
+const path = require('path');
+
+// own folder/file
+const userRoutes = require('./routes/user');
+
+// applications objects
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+app.get(bodyParser.urlencoded({extended:false}));
+//use route / paths /provide read rights
+app.use(express.static(path.join(__dirname,'public')));
+app.use(userRoutes);
+
+// 404 error page
+app.use((req,res,next) => {
+    //res.status(404).sendFile(path.join(__dirname,'views','404.html'));
+    res.status(404).render('404',{pageTitle: 'Page Not Found',
+    path: '/404' });
+});
+
+
+// main server - express build
+app.listen(8080);
