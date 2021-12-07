035ec5d6775408a6b733ab7e276ac4b195c63469
FoxHunterTx <foxhuntinttx@gmail.com>
2021-12-06 20:11:56 -0600

    ssl app.js

--- app.js
+++ app.js
@@ -1,13 +1,39 @@
 const http = require('http');
+const https = require('https');
 const express = require('express');
 const bodyParser = require('body-parser'); 
 const path = require('path');
+const fs = require('fs');
 
 // own folder/file
 const userRoutes = require('./routes/user');
+const { SSL_OP_ALL } = require('constants');
+
+// ssl setup
+const hostname = 'engineerbuddies.com';
+const httpPort = 80;
+const httpsPort = 443;
+const httpsOptions = {
+     cert: fs.readFileSync('./ssl/engineerbuddies.crt'),
+     ca: fs.readFileSync('./ssl/engineerbuddies.ca-bundle'),
+     key: fs.readFileSync('./ssl/engineerbuddies.key')
+};
 
 // applications objects
 const app = express();
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(httpsOptions,app);
+
+// reroute port 80
+app.use((req, res, next) => {
+    if (req.protocol === 'http') {
+        // console.log("http to https ");
+        // const httpsUrl = "https://" + req.headers.host + req.url;
+        console.log(httpsUrl);
+        res.redirect(301,httpsUrl);
+    } 
+      next();
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -25,6 +51,9 @@ app.use((req,res,next) => {
 
 
 // main server - express build added console log
-app.listen(80, function() {
-    console.log('Liberty Hill web server is up and running!')
+httpServer.listen(httpPort, function() {
+    console.log('rerouting request to https')
+});
+httpsServer.listen(httpsPort,function() {
+    console.log('starting up web server for engineeringbuddies.com')
 });
